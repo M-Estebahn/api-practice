d7e1c32b58f0d55486dbafeec6490758b71d3cb4
M-Estebahn <molina.estebahn@gmail.com>
2022-08-15 10:36:12 -0500

    more layout adjustments and pulling moves data from api

--- js/pokemon.js
+++ js/pokemon.js
@@ -6,12 +6,14 @@ $.get("https://pokeapi.co/api/v2/pokemon/").done(data=> {
         $.get(poke.url).done(pokemon=> {
             console.log(pokemon.name +' '+pokemon.id)
             $('#card-table').html(document.getElementById('card-table').innerHTML+
-                '<div class="small-card col-sm-12 col-md-4">'+
+                '<div class="small-card col-sm-12 col-md-3 ">'+
                 '<div>'+'<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+pokemon.id+'.png"' +' alt='+pokemon.name+'>'+'</div>'+
                 '<h3>'+pokemon.name+'</h3>'+
-                '<h4>'+'Abilities'+'</h4>'+
-                '<h5>'+pokemon.abilities[0].ability.name+'</h5>'+
-                '<h5>'+pokemon.abilities[1].ability.name+'</h5>'+
+                '<hr>'+
+                '<h4>'+'Moves:'+'</h4>'+
+                '<h5>'+pokemon.moves[0].move.name+'</h5>'+
+                '<h5>'+pokemon.moves[1].move.name+'</h5>'+
+                '<h5>'+pokemon.moves[2].move.name+'</h5>'+
             '</div>'
             );
 
