d59272f2a44569a42706137591048f487afa9b53
M-Estebahn <molina.estebahn@gmail.com>
2022-08-15 10:01:25 -0500

    working on layout with grid and flex through bootstrap

--- js/pokemon.js
+++ js/pokemon.js
@@ -6,14 +6,12 @@ $.get("https://pokeapi.co/api/v2/pokemon/").done(data=> {
         $.get(poke.url).done(pokemon=> {
             console.log(pokemon.name +' '+pokemon.id)
             $('#card-table').html(document.getElementById('card-table').innerHTML+
-                '<div class="small-card">'+
+                '<div class="small-card col-sm-12 col-md-4">'+
                 '<div>'+'<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+pokemon.id+'.png"' +' alt='+pokemon.name+'>'+'</div>'+
                 '<h3>'+pokemon.name+'</h3>'+
                 '<h4>'+'Abilities'+'</h4>'+
-                '<ol>'+
-                    +'<li>'+'Overgrow'+'</li>'+
-                +'<li>'+'Chlorophyll'+'</li>'+
-                '</ol>'+
+                '<h5>'+pokemon.abilities[0].ability.name+'</h5>'+
+                '<h5>'+pokemon.abilities[1].ability.name+'</h5>'+
             '</div>'
             );
 
