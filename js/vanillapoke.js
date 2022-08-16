71b435a696ad0d93fed2683c57678987d5492fde
M-Estebahn <molina.estebahn@gmail.com>
2022-08-16 09:20:58 -0500

    adding try catch to fix error and including html to render in vanilla JS code

--- js/vanillapoke.js
+++ js/vanillapoke.js
@@ -1,27 +1,38 @@
 "use strict";
 
-var pokeUrl= "https://pokeapi.co/api/v2/pokemon"
+var pokeUrl = "https://pokeapi.co/api/v2/pokemon"
 
-fetch(pokeUrl).then(data=> {
-    console.log(data)
-    console.log(data.json())
-    data.json().then(res=>{
+try {
+    fetch(pokeUrl)
+        .then(res => {
+            return res.json()
+        }).then(data => {
+        console.log(data.results)
+        data.results.forEach(result => {
+            fetch(result.url).then(pokemon => {
+                document.getElementById('card-table').innerHTML =
+                    '<div class="small-card col-sm-12 col-md-3 ">' +
+                    '<div>' + '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + pokemon.id + '.png"' + ' alt=' + pokemon.name + '>' + '</div>' +
+                    '<h3>' + pokemon.name + '</h3>' +
+                    '<hr>' +
+                    '<h4>' + 'Moves:' + '</h4>' +
+                    '<h5>' + pokemon.moves[0].move.name + '</h5>' +
+                    '<h5>' + pokemon.moves[1].move.name + '</h5>' +
+                    '<h5>' + pokemon.moves[2].move.name + '</h5>' +
+                    '</div>'
+            }).catch(err => {
+                console.log(err)
+            })
 
-    })
 
-   })
-//     res.results.forEach(result=>{
-//         fetch(result.url).then(pokemon=>{
-//             document.getElementById('card-table').innerHTML=
-//             '<div class="small-card col-sm-12 col-md-3 ">'+
-//             '<div>'+'<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+pokemon.id+'.png"' +' alt='+pokemon.name+'>'+'</div>'+
-//             '<h3>'+pokemon.name+'</h3>'+
-//             '<hr>'+
-//             '<h4>'+'Moves:'+'</h4>'+
-//             '<h5>'+pokemon.moves[0].move.name+'</h5>'+
-//             '<h5>'+pokemon.moves[1].move.name+'</h5>'+
-//             '<h5>'+pokemon.moves[2].move.name+'</h5>'+
-//             '</div>'
+        }
+    catch
+        (err)
+        {
+            console.log(err)
+        }
+
+
 //
 //         })
 //     })
