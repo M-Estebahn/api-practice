c22f0396d0caa78714f83e96dcde21de0cd24c66
M-Estebahn <molina.estebahn@gmail.com>
2022-08-16 10:45:50 -0500

    attempting to get through using a stored array for information and a loop.

--- js/vanillapoke.js
+++ js/vanillapoke.js
@@ -1,41 +1,47 @@
 "use strict";
 
-var pokeUrl = "https://pokeapi.co/api/v2/pokemon"
+//Attempt 1 trying to reach through 2 layers to get each pokemon. receiving  an undefined error not able to use fetch to get poke information on the second layer by using for each on the url recieved from the first layer.
 
-try {
-    fetch(pokeUrl)
-        .then(res => {
-            return res.json()
-        }).then(data => {
-        console.log(data.results)
-        data.results.forEach(result => {
-            fetch(result.url).then(pokemon => {
-                document.getElementById('card-table').innerHTML =
-                    '<div class="small-card col-sm-12 col-md-3 ">' +
-                    '<div>' + '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + pokemon.id + '.png"' + ' alt=' + pokemon.name + '>' + '</div>' +
-                    '<h3>' + pokemon.name + '</h3>' +
-                    '<hr>' +
-                    '<h4>' + 'Moves:' + '</h4>' +
-                    '<h5>' + pokemon.moves[0].move.name + '</h5>' +
-                    '<h5>' + pokemon.moves[1].move.name + '</h5>' +
-                    '<h5>' + pokemon.moves[2].move.name + '</h5>' +
-                    '</div>'
-            }).catch(err => {
-                console.log(err)
-            })
-
-
-        }
-    catch
-        (err)
-        {
-            console.log(err)
-        }
-
-
+// var pokeUrl = "https://pokeapi.co/api/v2/pokemon"
 //
-//         })
-//     })
-// }).catch(error=>{
-//     console.log(error)
-// })
+// try {
+//     fetch(pokeUrl)
+//         .then(res => {
+//             return res.json()
+//         }).then(data => {
+//         console.log(data.results)
+//         data.results.forEach(result => {
+//              fetch(result.url).then(pokemon => {
+//                 console.log(pokemon.name+" "+pokemon.id)
+
+            //     document.getElementById('card-table').innerHTML =
+            //         '<div class="small-card col-sm-12 col-md-3 ">' +
+            //         '<div>' + '<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/' + pokemon.id + '.png"' + ' alt=' + pokemon.name + '>' + '</div>' +
+            //         '<h3>' + pokemon.name + '</h3>' +
+            //         '<hr>' +
+            //         '<h4>' + 'Moves:' + '</h4>' +
+            //         '<h5>' + pokemon.moves[0].move.name + '</h5>' +
+            //         '<h5>' + pokemon.moves[1].move.name + '</h5>' +
+            //         '<h5>' + pokemon.moves[2].move.name + '</h5>' +
+            //         '</div>'
+    //         })
+    //     })
+    // })
+    //
+    // }catch(err){
+    //     console.log(err)}
+
+//Attempt 2: cycling through pokeinformation using a loop to grab each individually and then storing data in an array.
+
+var promises=[];
+
+for(let i=1;i<151;i++){
+    const url=`https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(url).then(res=>res.json()));
+}
+
+    Promise.all(promises).then(res=>
+    console.log(res))
+
+
+
